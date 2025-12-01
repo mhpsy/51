bd0ef1245bd0de045375be8f3b5534ee541b6593
mhpsy <mhpsy@proton.me>
2025-12-01 12:20:36 +0800

    串口通讯

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <utils/LCD1602.h>
 #include <utils/matrix_keypad.h>
 #include <utils/timer.h>
+#include <utils/UART.h>
 
 // LED 状态变量
 volatile unsigned char led_state = 0xFE; // 初始状态：0b11111110，P2.0 点亮
@@ -25,22 +26,27 @@ void button_handle_p33(void)
 {
 }
 
+unsigned char counter = 0;
+
 // 定时器回调函数，每1ms调用一次
 void Timer0_Callback(void)
 {
     // 注意：不要在这里重置 P2，否则会覆盖下面的逻辑
-    
+
     // 使用 timer.c 中的计数器
     if (Timer0_GetOverflowCount() >= 500) // 0.5秒钟
     {
         Timer0_ResetOverflowCount();
         // 切换LED状态
-        led_state = (led_state << 1) | 0x01; // 左移并补1
-        if (led_state == 0xFF)               // 如果全灭，重置为 P2.0 点亮
-        {
-            led_state = 0xFE;
-        }
-        P2 = led_state;
+        // led_state = (led_state << 1) | 0x01; // 左移并补1
+        // if (led_state == 0xFF)               // 如果全灭，重置为 P2.0 点亮
+        // {
+        //     led_state = 0xFE;
+        // }
+        // P2 = led_state;
+
+        // 发送计数器值通过 UART
+        UartSendByte(counter++);
     }
 }
 
@@ -50,12 +56,20 @@ void Timer0_ISR(void) __interrupt(1)
     Timer0_HandleISR();
 }
 
+void Uart_receive_ISR(void) __interrupt(4)
+{
+    if(RI == 0) return; // 如果不是接收中断，直接返回
+    P2=~SBUF; // 接收到的数据取反后显示在 P2 口
+    RI=0;     // 清除接收中断标志
+}
+
 void main()
 {
     P2 = 0xFE; // 先点亮第一个灯，证明程序跑起来了
     Timer0_SetCallback(Timer0_Callback);
     Timer0_Init();
-    
+    Uart_Init();
+
     while (1)
     {
     }
---
+++ src/utils/UART.c
@@ -0,0 +1,22 @@
+#include <mcs51/8052.h>
+
+void Uart_Init(void)	//4800bps@11.0592MHz
+{
+	PCON &= 0x7F;		//波特率不倍速
+	SCON = 0x50;		//8位数据,可变波特率
+	TMOD &= 0x0F;		//设置定时器模式
+	TMOD |= 0x20;		//设置定时器模式
+	TL1 = 0xFA;			//设置定时初始值
+	TH1 = 0xFA;			//设置定时重载值
+	ET1 = 0;			//禁止定时器中断
+	TR1 = 1;			//定时器1开始计时
+	EA=1;
+	ES=1;
+}
+
+void UartSendByte(unsigned char byte)
+{
+    SBUF = byte;
+    while (TI==0);
+    TI = 0;
+}
